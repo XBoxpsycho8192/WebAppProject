21e3acea84aac3b6a70d6c574245cdf6643e48f8
mjcrow <mjcrow001@gmail.com>
2023-06-17 19:25:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,22 +16,22 @@ inventory = [
     {"name": "Hammer", "price": "24.99", "department": "Hardware", "inv": "0001"},
 ]
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/search', methods=['POST'])
+@app.route("/search", methods=["POST"])
 def search():
     # Get the user input from the form
-    search = request.form.get('search')
+    search = request.form.get("search")
 
     # Search for matching objects
     match = []
     for obj in inventory:
-        if search.lower() in obj['name'].lower():
+        if search.lower() in obj["name"].lower():
             match.append(obj)
 
-    return render_template('results.html', match=match)
+    return render_template("results.html", match=match)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
