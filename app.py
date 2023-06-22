79b9ebb1eca2709adf1923a388de69e871586fb9
mjcrow <mjcrow001@gmail.com>
2023-06-21 23:54:22 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask
+from views import views
 
 app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/")
 
-
-inventory = [
-    {"name": "Wrench", "price": "24.99", "department": "Hardware", "inv": "0001"},
-    {"name": "Shirt", "price": "14.99", "department": "Clothes", "inv": "0001"},
-    {"name": "Pants", "price": "34.99", "department": "Clothes", "inv": "0001"},
-    {"name": "TV", "price": "699.99", "department": "Electronics", "inv": "0001"},
-    {"name": "Laptop", "price": "899.99", "department": "Electronics", "inv": "0001"},
-    {"name": "Saw", "price": "14.99", "department": "Hardware", "inv": "0001"},
-    {"name": "Blender", "price": "24.99", "department": "Housewares", "inv": "0001"},
-    {"name": "Waffle Iron", "price": "29.99", "department": "Housewares", "inv": "0001"},
-    {"name": "Socks", "price": "4.99", "department": "Hardware", "inv": "0001"},
-    {"name": "Hammer", "price": "24.99", "department": "Hardware", "inv": "0001"},
-]
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/search", methods=["POST"])
-def search():
-    # Get the user input from the form
-    search = request.form.get("search")
-
-    # Search for matching objects
-    match = []
-    for obj in inventory:
-        if search.lower() in obj["name"].lower():
-            match.append(obj)
-
-    return render_template("results.html", match=match)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# This file is what actually starts and runs the web app.
+if __name__=='__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
--- views.py
+++ views.py
@@ -1,43 +1,74 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from inventory import inventory
+from inventory import departments, add_product, save_inventory, edit_inventory
 
+# This file serves as a route map. It tells flask which webpage to load.
 views = Blueprint(__name__, "views")
 
+
+# Alot of these routes are for experiments right now.
+# This is the route to the Home Page.
 @views.route("/")
 def home():
     return render_template("index.html")
 
-@views.route("results")
-def results():
-    return render_template("results.html")
 
+@views.route("go-to-home")
+def go_to_home():
+    return redirect(url_for("views.home"))
+
+
+# This is the route to the Profile Page.
+@views.route("profile_page")
+def profile_page():
+    return render_template("profile.html")
+
+
+@views.route("inventory_page")
+def inventory_page():
+    return render_template("inventory.html", inventory=inventory)
+
+
+@views.route("/add_product", methods=["GET", "POST"])
+def product_add():
+    if request.method == "POST":
+        name = request.form.get('name')
+        price = float(request.form.get('price'))
+        department = request.form.get('department')
+        quantity = int(request.form.get('quantity'))
+
+        add_product(name, price, department, quantity)
+
+        return redirect(url_for("views.inventory_page"))
+
+    return render_template("add_product.html", departments=departments)
+
+
+@views.route("/save_inventory")
+def inventory_save():
+    save_inventory()
+    return redirect(url_for("views.inventory_page"))
+
+
+@views.route("/edit_inventory", methods=["GET", "POST"])
+def inventory_edit():
+    if request.method == "POST":
+        sku = request.form.get('sku')
+        quantity = int(request.form.get('quantity'))
+        edit_inventory(sku, quantity)
+        return redirect(url_for("views.inventory_page"))
+    return render_template("edit_inventory.html")
+
+# search route
 @views.route("/search", methods=["POST"])
 def search():
-    input = request.form["input"]
-    match = None
+    # Get the user input from the form
+    search = request.form.get("search")
 
-    # Search through the array of objects
+    # Search for matching objects
+    match = []
     for obj in inventory:
-        if input == obj["name"]:
-            match = obj
-            break
-    # fill template with matched object
-    return render_template("views/results.html", match = match)
+        if search.lower() in obj["name"].lower():
+            match.append(obj)
 
-# constructor
-class Item:
-    def __init__(self, name, price, department, stock):
-        self.name = name
-        self.price = price
-        self.department = department
-        self.stock = stock
-
-# create objects
-wrench = Item("wrench", 19.99, "hardware", 2929)
-shirt = Item("shirt", 5.99, "clothing", 5722)
-laptop = Item("laptop", 799.99, "electronics", 1222)
-tv = Item("tv", 499.99, "electronics", 9222)
-pants = Item("pants", 29.99, "clothing", 2212)
-
-# create test array
-inventory = [wrench, shirt, laptop, tv, pants]
-
+    return render_template("results.html", match=match)
