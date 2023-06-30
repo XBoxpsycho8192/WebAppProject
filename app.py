0f051f26aae8c86cb64fea33698f335baa72dd02
XBoxpsycho8192 <pattersonwill8192@hotmail.com>
2023-06-29 23:30:31 -0400

    HTTPS added

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def create_tables():
 
 # This file is what actually starts and runs the web app.
 if __name__=='__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000, ssl_context=('cert.pem', 'key.pem'))
